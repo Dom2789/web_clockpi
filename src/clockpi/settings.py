448cdaa6470b481800fa088c385b786ff0efce4d
Dominik Hoch <dominik.hoch.1989@freenet.de>
2026-03-07 20:43:36 +0100

    settings.py modifed so webserver can be started on mac

--- src/clockpi/settings.py
+++ src/clockpi/settings.py
@@ -10,6 +10,7 @@ import logging
 
 # path to config file
 CONFIG_PATH = "/home/pi/_config/Webserver.txt"
+CONFIG_PATH = "/Users/dom/temp/Webserver.txt"
 config = Config(CONFIG_PATH)
 
 # settings located in external config-file
